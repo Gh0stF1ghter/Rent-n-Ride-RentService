345e84a3dde279c4d178d90d4bf4ca94081ad617
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-06-24 17:00:17 +0300

    feat: add entity

---
+++ Rent.DataAccess/Entities/VehicleClientHistoryEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Rent.DataAccess.Entities;
+
+public class VehicleClientHistoryEntity
+{
+    public Guid Id { get; set; }
+
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+
+    public Guid VehicleId { get; set; }
+
+    public Guid ClientId { get; set; }
+}
