226a882edc1f6997c10c24074b1ba2cb5910b12e
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-02 10:56:19 +0300

    Feature/add api layer (#8)
    * feat: add api layer
    
    * feat: implement viewModels
    
    * feat: implement controller
    
    * feat: implement exception handling middleware
    
    * feat: configure swagger for autherization
    
    * feat: add scope handler for Auth0 support
    
    * feat: add validation
    
    * feat: add DI
    
    * feat: add docker support
    
    * feat: add configuration params
    
    * feat: add efcore.design package
    
    * feat: add authorization permission for Delete Endpoint
    
    * Update sonarcloud.yml
    
    * feat: add exception in exception handler
    
    * Update sonarcloud.yml

---
+++ Rent.API/Extensions/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Rent.API.Exceptions;
+using Rent.BusinessLogic.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace Rent.API.Extensions;
+
+public class ExceptionHandlingMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception ex)
+    {
+        var responseMessage = ex switch
+        {
+            NotFoundException => new ExceptionResponse(
+                (int)HttpStatusCode.NotFound,
+                ex.Message
+                ),
+
+            InvalidOperationException => new ExceptionResponse(
+                (int)HttpStatusCode.NotFound,
+                ex.Message
+                ),
+
+            InvalidDataException => new ExceptionResponse(
+                (int)HttpStatusCode.BadRequest,
+                ex.Message
+                ),
+
+            _ => new ExceptionResponse(
+                (int)HttpStatusCode.InternalServerError,
+                ex.Message
+            )
+        };
+
+        var response = JsonSerializer.Serialize(responseMessage);
+
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
