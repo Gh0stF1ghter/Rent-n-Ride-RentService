ab712c4b73e50d656c143249b7f9c3132f0af89b
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-03 17:02:42 +0300

    feat: add model replicas

---
+++ Rent.BusinessLogic/Models/ClientModel.cs
@@ -0,0 +1,11 @@
+namespace Rent.BusinessLogic.Models;
+
+public class ClientModel
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public decimal Balance { get; set; }
+}
---
+++ Rent.BusinessLogic/Models/VehicleModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Rent.BusinessLogic.Models;
+
+public class VehicleModel
+{
+    public Guid Id { get; set; }
+    public decimal RentCost { get; set; }
+}
