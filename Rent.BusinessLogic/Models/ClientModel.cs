7648b35a56d26204c3f37a74df8bab1029753451
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-05 14:33:30 +0300

    feat: add props for service models

--- Rent.BusinessLogic/Models/ClientModel.cs
+++ Rent.BusinessLogic/Models/ClientModel.cs
@@ -8,4 +8,5 @@ public class ClientModel
     public string Email { get; set; } = string.Empty;
     public string PhoneNumber { get; set; } = string.Empty;
     public decimal Balance { get; set; }
+    public bool IsRenting { get; set; }
 }
