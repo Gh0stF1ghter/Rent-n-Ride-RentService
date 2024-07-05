0b79885c993271e9766c8a8f49983cccbb0fe5df
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-05 14:32:56 +0300

    feat: add exception messages builders

--- Rent.BusinessLogic/Exceptions/ExceptionMessages/ExceptionMessages.cs
+++ Rent.BusinessLogic/Exceptions/ExceptionMessages/ExceptionMessages.cs
@@ -4,4 +4,21 @@ internal static class ExceptionMessages
 {
     public static string NotFound(string entityType, Guid guid) =>
         $"{entityType} with id:{guid} was not found";
+
+    public static string InsufficientFunds(Guid userId, decimal userBalance, decimal vehicleCost) =>
+        $"User with id:{userId} does not have enough funds on balance. User: {userBalance}, Vehicle: {vehicleCost}";
+    public static string NewEndDateLessThanCurrent(DateTime newEndDateTime, DateTime currentEndDateTime) =>
+        $"New end datetime is greater than updated ({newEndDateTime} against {currentEndDateTime})";
+
+    public static string NotFoundInService(string entityType, string? connection) =>
+        $"{entityType} was not found while requesting {connection}";
+
+    public static string UserIsRenting(Guid id) =>
+        $"User ({id}) is already renting a vehicle";
+
+    public static string VehicleIsRented(Guid id) =>
+        $"Vehicle ({id}) is already rented";
+
+    public static string ServiceError(string? connection) =>
+        "Something went wrong during request: " + connection;
 }
