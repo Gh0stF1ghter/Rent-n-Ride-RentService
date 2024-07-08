bac2374d701517d492639c6664d4c786f3f30fe4
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-08 13:04:05 +0300

    refactor: adjust configurationMock

--- Rent.UnitTests/Mocks/СonfigurationMock.cs
+++ Rent.UnitTests/Mocks/СonfigurationMock.cs
@@ -1,14 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Moq;
+using Rent.BusinessLogic.Models;
 
 namespace Rent.UnitTests.Mocks;
 internal class ConfigurationMock : Mock<IConfiguration>
 {
     public void GetUserConnection() =>
         Setup(c => c.GetSection("ConnectionStrings")["UserServiceConnection"])
-        .Returns("http://localhost:5054/api/client/");
+        .Returns(nameof(ClientModel));
 
     public void GetCatalogueCoonnection() =>
         Setup(c => c.GetSection("ConnectionStrings")["CatalogueServiceConnection"])
-        .Returns("http://localhost:5131/api/vehicle/");
+        .Returns(nameof(VehicleModel));
 }
