fd6897168e802f20d75b0b10461add691bbf9b20
Dmitriy Gorchanin <dmitriygorchanin@gmail.com>
2024-07-16 13:08:23 +0300

    feat: add consumers

---
+++ Rent.BusinessLogic/Consumers/VehicleClientHistoryCreatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.VehicleClientHistoryEvents;
+using Mapster;
+using MassTransit;
+using Rent.BusinessLogic.Models;
+using Rent.BusinessLogic.Services.Interfaces;
+
+namespace Rent.BusinessLogic.Consumers;
+
+public class VehicleClientHistoryCreatedConsumer(IVehicleClientHistoryService service) : IConsumer<VehicleClientHistoryCreated>
+{
+    public async Task Consume(ConsumeContext<VehicleClientHistoryCreated> context)
+    {
+        var vehicleClientHistoryFromEvent = context.Message;
+
+        await Console.Out.WriteLineAsync($"vehicle client history {vehicleClientHistoryFromEvent.VehicleId} consumed to create");
+
+        var vehicleClientHistory = vehicleClientHistoryFromEvent.Adapt<VehicleClientHistoryModel>();
+
+        await service.AddAsync(vehicleClientHistory, default);
+    }
+}
---
+++ Rent.BusinessLogic/Consumers/VehicleClientHistoryUpdatedConsumer.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.VehicleClientHistoryEvents;
+using Mapster;
+using MassTransit;
+using Rent.BusinessLogic.Models;
+using Rent.BusinessLogic.Services.Interfaces;
+
+namespace Rent.BusinessLogic.Consumers;
+
+public class VehicleClientHistoryUpdatedConsumer(IVehicleClientHistoryService service) : IConsumer<VehicleClientHistoryUpdated>
+{
+    public async Task Consume(ConsumeContext<VehicleClientHistoryUpdated> context)
+    {
+        var vehicleClientHistoryFromEvent = context.Message;
+
+        await Console.Out.WriteLineAsync($"vehicle client history {vehicleClientHistoryFromEvent.VehicleId} consumed to create");
+
+        var vehicleClientHistory = vehicleClientHistoryFromEvent.Adapt<VehicleClientHistoryModel>();
+
+        await service.UpdateAsync(vehicleClientHistory, default);
+    }
+}
